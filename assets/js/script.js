e9d3f65b16bf62e89fe773d717b233b8018f3e9d
toan10921 <33760859+toan10921@users.noreply.github.com>
2019-10-24 21:51:49 +0700

    toan update code
    toan update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,11 @@ $(document).ready(function () {
     if($(".js_banner-slider").length > 0){
         $(".js_banner-slider").each(function () {
             $(this).owlCarousel({
-                items: 1
+                items: 1,
+                nav: true,
+                loop: true,
+                // animateIn: "fadeIn",
+                // animateOut: "fadeOut"
             });
         });
     }
