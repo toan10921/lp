e726f664d0035c3fc75f6b458e8daa246934d257
toan10921 <33760859+toan10921@users.noreply.github.com>
2019-10-27 06:45:08 +0700

    toan update code
    toan update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,6 +59,20 @@ $(document).ready(function () {
         });
     }
 
+    if($(".js_doitac-slider").length > 0){
+        $(".js_doitac-slider").each(function (){
+            $(this).owlCarousel({
+                items: 6,
+                nav: true,
+                dots: false,
+                loop: true,
+                navText: ['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
+                animateIn: "slideInLeft",
+                animateOut: "slideOutLeft"
+            });
+        });
+    }
+
 });
 
 $(window).on("load",function () {
