42e4e5345b93b48114fc8ece809ff795878a4ffc
toan10921 <33760859+toan10921@users.noreply.github.com>
2019-10-29 18:05:20 +0700

    toan update code
    toan update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,6 +73,40 @@ $(document).ready(function () {
         });
     }
 
+    if($(".js_thu-vien-youtube-slider").length > 0){
+        $(".js_thu-vien-youtube-slider").each(function (){
+            $(this).owlCarousel({
+                items: 3,
+                nav: false,
+                dots: true,
+                loop: true,
+                navText: ['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
+                animateIn: "slideInLeft",
+                animateOut: "slideOutLeft"
+            });
+        });
+    }
+
+    if($(".famous-people-outer .infor-famous").length > 0){
+        $(".famous-people-outer .infor-famous").each(function () {
+           $(this).find("li a").on("click",function (event) {
+               event.preventDefault();
+               var src_img = $(this).data("image");
+               $(this).parents(".famous-people-outer").find(".img-famous img").attr("src",src_img).hide().fadeIn();
+               $(this).parents(".famous-people-wrap").find(".img-bg-wrap").css("background","url("+src_img+")");
+           }) ;
+        });
+    }
+
+    if($(".chuyen-nganh-wrap").length > 0){
+        $(".js_chuyen-nganh-wrap").each(function () {
+            $(this).find(".col-title .nav li a").on("click",function () {
+               $(this).parents(".col-title").find(" .nav li .title-content").slideUp("fast");
+               $(this).parents("li").find(".title-content").slideDown("fast");
+            });
+        });
+    }
+
 });
 
 $(window).on("load",function () {
