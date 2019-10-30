d01cb5750b7f3fc2a8419cb76d439a2ee86931ea
toan10921 <33760859+toan10921@users.noreply.github.com>
2019-10-30 18:09:06 +0700

    toan update code
    toan update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,7 @@
 "use strict";
 
 $(document).ready(function () {
-    if($(".js_banner-slider").length > 0){
-        $(".js_banner-slider").each(function () {
-            $(this).owlCarousel({
-                items: 1,
-                nav: true,
-                loop: true,
-                navText: ['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-                animateIn: "fadeIn",
-                animateOut: "fadeOut"
-            });
-        });
-        // Fix height slider
-        $('.js_banner-slider .banner-info').each(function () {
-            if ($(this).find('.slider-content-text').length > 0) {
-                var height_content = $(this).find('.slider-content-text')["0"].clientHeight;
-                $(this).css('height', height_content);
-            }
-        })
-    }
+
     if($(".js_sinhvien-slider").length > 0){
         $(".js_sinhvien-slider").each(function (){
             $(this).owlCarousel({
